de140f653a11c9ab2ca4047a4362f12db3feed4d
R4NP3R <ssrafa.2014@gmail.com>
2024-12-25 16:08:37 -0300

    Alterando index para fazer o migrate do knex

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { Knex } from "./server/databases/knex";
 import { server } from "./server/Server";
 
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`API iniciada na porta: ${process.env.PORT || 3333}`)
+  })
+}
 
-
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`API iniciada na porta: ${process.env.PORT || 3333}`)
-})
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest().then(() => {
+    startServer()
+  })
+} else {
+  startServer()
+}
